45c35163d7d77c33008b597e5021b26e7d8961cc
sweetykr7 <71308690+sweetykr7@users.noreply.github.com>
2021-11-17 06:25:16 +0900

    Update coin_cf.py

--- CoinTrading/library/coin_cf.py
+++ CoinTrading/library/coin_cf.py
@@ -5,14 +5,14 @@ import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 
-db_id='bot'
+db_id=''
 # db ip
 db_ip='localhost'
 # db 패스워드
-db_passwd='tyddlelql7!$!$'
+db_passwd=''
 
 # db port가 3306이 아닌 다른 port를 사용 하시는 분은 아래 변수에 포트에 맞게 수정하셔야합니다.
-db_port='3306'
+db_port=''
 
 start_date='20100101'
 
